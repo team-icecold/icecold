52cd31d8f1c9e29fead0070fea3c281069f1c268
adamchappell00 <adam.chappell00@gmail.com>
2022-04-21 08:01:53 -0500

    init

---
+++ src/main/java/com/example/icecold/IcecoldApplication.java
@@ -0,0 +1,13 @@
+package com.example.icecold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcecoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcecoldApplication.class, args);
+    }
+
+}

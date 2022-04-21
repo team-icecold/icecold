4b870fc1e1d21ad04de2cf722176d5c3835d8df0
adamchappell00 <adam.chappell00@gmail.com>
2022-04-21 08:38:36 -0500

    first mappings and pages added

--- src/main/java/com/example/icecold/controllers/UserController.java
+++ src/main/java/com/example/icecold/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.example.icecold.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
@@ -11,9 +12,19 @@ public class UserController {
     private UserRepository usersDao;
     private PasswordEncoder passwordEncoder;
 
-
     public UserController(UserRepository usersDao, PasswordEncoder passwordEncoder){
         this.usersDao = usersDao;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "users/login";
+    }
+
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "users/dashboard";
     }
 }
